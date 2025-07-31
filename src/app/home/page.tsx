13fa8bfd6093e5801303e99e7b23588777559350
NandhuNarayanan <nandhunaznarayan@gmail.com>
2025-07-31 19:36:23 +0530

    banner changed to new component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,6 @@
 import { BookingSection } from "@/components/BookingSection";
+import { EarningsCard } from "@/components/EarningsCard";
+import { MainBanner } from "@/components/MainBanner";
 import { ProductsSection } from "@/components/ProductsSection";
 import { ServiceSection } from "@/components/ServiceSection";
 import { ShineSection } from "@/components/ShineSection";
@@ -10,69 +12,10 @@ export default function CarServiceApp() {
   return (
     <div className="max-w-sm mx-auto bg-[#F5F5F5] min-h-screen overflow-y-scroll no-scrollbar pb-30 pt-5">
       {/* Main Banner */}
-      <div className="mx-4 mb-4 bg-gradient-to-r from-green-400 to-green-500 rounded-2xl p-4 relative overflow-hidden">
-        <div className="flex justify-between items-center">
-          <div className="flex-1">
-            <h2 className="text-white text-lg font-bold mb-1">
-              Keep Your Car Spotless!
-            </h2>
-            <p className="text-white text-sm mb-3 leading-relaxed">
-              Get basic cleaning at doorstep in flat rate and fast service. Book
-              now to get a sparkling clean ride.
-            </p>
-            <button className="flex justify-center items-center bg-white text-green-600 px-4 py-2 rounded-full text-sm font-medium">
-              Book Now
-              <svg
-                className="w-6 h-6 ml-2"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M14 5l7 7m0 0l-7 7m7-7H3"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div className="ml-4">
-            <Image
-              src="/assets/illustations/car_home.svg"
-              alt="icon"
-              width={120}
-              height={80}
-            />
-          </div>
-        </div>
-      </div>
+      <MainBanner />
 
-      {/* Earnings Card */}
-      <div className="mx-4 mb-4 bg-[#FFFFFF] rounded-lg p-3 flex items-center justify-between">
-        <div className="flex flex-col ">
-          <div className=" rounded flex items-center justify-center mr-3">
-            <span className="text-[#128C7E] text-sm">
-              Your Shinr Cash Balance
-            </span>
-          </div>
-          <span className="text-[#083B35] font-semibold font-weight-600">
-            ₹
-            <span className="text-[#083B35] font-semibold font-weight-600">
-              2000
-            </span>
-          </span>
-        </div>
-        <div className="rounded flex items-center justify-center mr-3">
-          <Image
-            src="/assets/illustations/wallet.svg"
-            alt="icon"
-            width={34}
-            height={34}
-          />
-        </div>
-      </div>
+      {/*Earnings Card*/}
+      <EarningsCard />
 
       {/* Service Section */}
       <ServiceSection />
