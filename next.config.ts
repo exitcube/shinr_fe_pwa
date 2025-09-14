cc315a286e513992acf49214713794ddc4bc5f66
Ahmad7420 <ahmadabdulrahman6@gmail.com>
2025-09-14 19:09:03 +0400

    pwa pending

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import createNextPWA from "next-pwa";
+
+const withPWA = createNextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NEXT_PUBLIC_ENABLE_PWA !== "true",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
