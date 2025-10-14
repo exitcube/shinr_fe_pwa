97613da20dcd50f07c4e50f562fc105b68c4b414
Ahmad Abdul Rahiman <ahmad.salinagroup.net>
2025-10-14 12:31:50 +0400

    .

--- src/provider/AuthProvider.tsx
+++ src/provider/AuthProvider.tsx
@@ -87,7 +87,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 };
 
 // Hook for components to use auth
-export const useAuth = () => {
+export const useAuthContext = () => {
   const context = useContext(AuthContext);
   if (!context) throw new Error("useAuth must be used inside AuthProvider");
   return context;
